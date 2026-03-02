97ab57ea984487ed7cfb3bf335ca0d506332b949
cjimenez909 <cjime21@uic.edu>
2026-03-02 14:27:04 -0600

    added component with greeting and image

---
+++ app/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hello() {
+    return (
+        <div> 
+            <header>Hi, my name is Claudia</header>
+            <p>Welcome to my personal website!</p>
+            <img src="/newpic_small.png"></img>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Hello from "./components/Hello"
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
       <Link href="/projects">Projects</Link>
+      <Hello></Hello>
     </div>
   );
 }
